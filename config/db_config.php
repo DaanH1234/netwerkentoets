7fe0f42978bcc8c47f0587d62da27832124dacee
DaanH123 <daan.hendriksen1@student.graafschapcollege.nl>
2024-04-09 09:13:42 +0200

    test

--- config/db_config.php
+++ config/db_config.php
@@ -1,9 +1,9 @@
 <?php
 
 $dbhost = 'localhost:3306';
-$dbuser = 'net24dhendriksen_net24dhendriksen_netwerkentoet';
-$dbpass = 'C3ks]D=JFq@X';
-$dbname = 'net24dhendriksen_netwerken_2024';
+$dbuser = 'root';
+$dbpass = '';
+$dbname = 'netwerken_2024';
 
 $conn = new mysqli($dbhost,$dbuser,$dbpass,$dbname);
 if($conn->connect_error)
